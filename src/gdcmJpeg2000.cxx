d92be82d301c24a42e894d1d40b2b2c7173b1032
jpr <jpr>
2003-07-23 10:18:00 +0000

    Add empty gdcmJpeg2000.cxx, ready for the future

---
+++ src/gdcmJpeg2000.cxx
@@ -0,0 +1,20 @@
+ 
+ 
+ /* -------------------------------------------------------------------- */
+ //
+ // JPEG 2000 Files
+ //
+ /* -------------------------------------------------------------------- */
+
+#include <stdio.h>
+#include "gdcmFile.h"
+
+int
+gdcmFile::gdcm_read_JPEG2000_file (void * image_buffer) {
+
+
+   printf("Sorry JPEG 2000 File not yet taken into account\n");
+   return 0;
+}
+
+
