f4a2bc0de76ab784a57516c97e1ecd48a1040c1c
jpr <jpr>
2003-07-23 09:27:26 +0000

    adding jpeg12 bits .h file , in libijg12. I hope, this time ...

---
+++ src/jpeg/libijg12/jBitsInJsample12.h
@@ -0,0 +1,4 @@
+#define BITS_IN_JSAMPLE  12	/* use 8 or 12 */
+
+// to be used from dcmlib.
+// to be removed when both jpeg8 bits and jpeg12 bits are usable
---
+++ src/jpeg/libijg12/jconfig12.h
@@ -0,0 +1,7 @@
+#ifdef _MSC_VER
+   #include "jconfig.vc"
+#endif
+#ifdef __GNUC__
+   #include "jconfig.linux"
+#endif
+
