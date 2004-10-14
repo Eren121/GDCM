05a31743c6545f03a9841531f2d54b7b1552e681
malaterre <malaterre>
2004-10-14 22:16:33 +0000

    ENH: Afterall change my mind we need a common place: gdcmJpeg to define the reading of jpeg file, then gdcmJpeg[8,12,16] are just a kludge because of this compile time option in ijg

---
+++ src/gdcmJpeg16.cxx
@@ -0,0 +1,30 @@
+/*=========================================================================
+                                                                                
+  Program:   gdcm
+  Module:    $RCSfile: gdcmJpeg16.cxx,v $
+  Language:  C++
+  Date:      $Date: 2004/10/14 22:16:33 $
+  Version:   $Revision: 1.1 $
+                                                                                
+  Copyright (c) CREATIS (Centre de Recherche et d'Applications en Traitement de
+  l'Image). All rights reserved. See Doc/License.txt or
+  http://www.creatis.insa-lyon.fr/Public/Gdcm/License.html for details.
+                                                                                
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+                                                                                
+=========================================================================*/
+
+#include "gdcmFile.h"
+
+extern "C" {
+#include "src/jpeg/libijg16/jconfig.h"
+#include "src/jpeg/libijg16/jpeglib.h"
+}
+
+#define gdcm_write_JPEG_file gdcm_write_JPEG_file16
+#define gdcm_read_JPEG_file  gdcm_read_JPEG_file16
+
+#include "gdcmJpeg.cxx"
+
