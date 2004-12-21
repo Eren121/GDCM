e550e14aaf7d0223bbb18afe26ae1da2cfbbd332
malaterre <malaterre>
2004-12-21 15:16:07 +0000

    ENH: Fixing another warning about unused var

--- src/gdcmBase.cxx
+++ src/gdcmBase.cxx
@@ -3,8 +3,8 @@
   Program:   gdcm
   Module:    $RCSfile: gdcmBase.cxx,v $
   Language:  C++
-  Date:      $Date: 2004/12/16 13:46:38 $
-  Version:   $Revision: 1.1 $
+  Date:      $Date: 2004/12/21 15:16:07 $
+  Version:   $Revision: 1.2 $
                                                                                 
   Copyright (c) CREATIS (Centre de Recherche et d'Applications en Traitement de
   l'Image). All rights reserved. See Doc/License.txt or
@@ -45,7 +45,7 @@ Base::~Base()
  * \brief   Print all the object
  * @param   os The output stream to be written to.
  */
-void Base::Print(std::ostream &os)
+void Base::Print(std::ostream &)
 {
 }
 
