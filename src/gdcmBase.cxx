3e98859eed8220c1c378bb9ae839f63bbcfef7f9
malaterre <malaterre>
2005-01-06 20:25:17 +0000

    COMP: remove warning

--- src/gdcmBase.cxx
+++ src/gdcmBase.cxx
@@ -3,8 +3,8 @@
   Program:   gdcm
   Module:    $RCSfile: gdcmBase.cxx,v $
   Language:  C++
-  Date:      $Date: 2005/01/06 20:03:26 $
-  Version:   $Revision: 1.4 $
+  Date:      $Date: 2005/01/06 20:25:17 $
+  Version:   $Revision: 1.5 $
                                                                                 
   Copyright (c) CREATIS (Centre de Recherche et d'Applications en Traitement de
   l'Image). All rights reserved. See Doc/License.txt or
@@ -45,7 +45,7 @@ Base::~Base()
  * \brief   Print all the object
  * @param   os The output stream to be written to.
  */
-void Base::Print(std::ostream &os)
+void Base::Print(std::ostream &)
 {
 }
 
