ab3dc69a71af640fa49cbd917272d9ff7d1651b6
jpr <jpr>
2005-01-26 14:56:38 +0000

    * remove ContentEntry::Write() code, yet the method is 'pure virtual' 	  (coverage will be better ;-)

--- src/gdcmContentEntry.cxx
+++ src/gdcmContentEntry.cxx
@@ -3,8 +3,8 @@
   Program:   gdcm
   Module:    $RCSfile: gdcmContentEntry.cxx,v $
   Language:  C++
-  Date:      $Date: 2005/01/26 10:29:16 $
-  Version:   $Revision: 1.4 $
+  Date:      $Date: 2005/01/26 14:56:38 $
+  Version:   $Revision: 1.5 $
                                                                                 
   Copyright (c) CREATIS (Centre de Recherche et d'Applications en Traitement de
   l'Image). All rights reserved. See Doc/License.txt or
@@ -58,60 +58,6 @@ ContentEntry::~ContentEntry ()
 
 //-----------------------------------------------------------------------------
 // Public
-
-/**
- * \brief   Writes the 'std::string representable' value of a ContentEntry
- * @param fp already open ofstream pointer
- * @param filetype type of the file (ACR, ImplicitVR, ExplicitVR, ...)
- */
-void ContentEntry::WriteContent(std::ofstream *fp, FileType filetype)
-{
-   DocEntry::WriteContent(fp, filetype);
-
-   if ( GetGroup() == 0xfffe )
-   {
-      return; //delimitors have NO value
-   }
-
-   const VRKey &vr = GetVR();
-   unsigned int lgr = GetLength();
-   if (vr == "US" || vr == "SS")
-   {
-      // some 'Short integer' fields may be multivaluated
-      // each single value is separated from the next one by '\'
-      // we split the string and write each value as a short int
-      std::vector<std::string> tokens;
-      tokens.erase(tokens.begin(),tokens.end()); // clean any previous value
-      Util::Tokenize (GetValue(), tokens, "\\");
-      for (unsigned int i=0; i<tokens.size();i++)
-      {
-         uint16_t val_uint16 = atoi(tokens[i].c_str());
-         binary_write( *fp, val_uint16);
-      }
-      tokens.clear();
-      return;
-   }
-   if (vr == "UL" || vr == "SL")
-   {
-      // Some 'Integer' fields may be multivaluated (multiple instances 
-      // of integer). But each single integer value is separated from the
-      // next one by '\' (backslash character). Hence we split the string
-      // along the '\' and write each value as an int:
-      std::vector<std::string> tokens;
-      tokens.erase(tokens.begin(),tokens.end()); // clean any previous value
-      Util::Tokenize (GetValue(), tokens, "\\");
-      for (unsigned int i=0; i<tokens.size();i++)
-      {
-         uint32_t val_uint32 = atoi(tokens[i].c_str());
-         binary_write( *fp, val_uint32);
-      }
-      tokens.clear();
-      return;
-   } 
-
-   gdcmAssertMacro( lgr == GetValue().length() );
-   binary_write(*fp, GetValue());
-} 
 
 //-----------------------------------------------------------------------------
 // Protected
