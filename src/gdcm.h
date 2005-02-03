ccf6384353d152e9c8964d91f38177c01469cb54
manu <manu>
2005-02-03 16:08:35 +0000

    FIX: replaced gdcmSerieHeader.h by gdcmSerieHelper.h

--- src/gdcm.h
+++ src/gdcm.h
@@ -3,8 +3,8 @@
   Program:   gdcm
   Module:    $RCSfile: gdcm.h,v $
   Language:  C++
-  Date:      $Date: 2005/01/24 14:14:10 $
-  Version:   $Revision: 1.52 $
+  Date:      $Date: 2005/02/03 16:08:35 $
+  Version:   $Revision: 1.53 $
                                                                                 
   Copyright (c) CREATIS (Centre de Recherche et d'Applications en Traitement de
   l'Image). All rights reserved. See Doc/License.txt or
@@ -31,7 +31,7 @@
 #include "gdcmDocument.h"
 #include "gdcmFile.h"
 #include "gdcmFileHelper.h"
-#include "gdcmSerieHeader.h"
+#include "gdcmSerieHelper.h"
 
 #include "gdcmUtil.h"
 
