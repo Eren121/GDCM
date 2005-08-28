ac113b082275780715dbd1605b2d7c873481eb9a
jpr <jpr>
2005-08-28 17:02:34 +0000

    PrintFile can now print *all* the file within a given directory. (usefull to grep the result, when looking for an oddity)

--- Example/PrintFile.cxx
+++ Example/PrintFile.cxx
@@ -3,8 +3,8 @@
   Program:   gdcm
   Module:    $RCSfile: PrintFile.cxx,v $
   Language:  C++
-  Date:      $Date: 2005/07/20 13:39:51 $
-  Version:   $Revision: 1.46 $
+  Date:      $Date: 2005/08/28 17:02:34 $
+  Version:   $Revision: 1.47 $
                                                                                 
   Copyright (c) CREATIS (Centre de Recherche et d'Applications en Traitement de
   l'Image). All rights reserved. See Doc/License.txt or
@@ -22,6 +22,7 @@
 
 #include "gdcmFileHelper.h"
 #include "gdcmDebug.h"
+#include "gdcmDirList.h"
 
 #include "gdcmArgMgr.h"
 
@@ -114,21 +115,21 @@ void ShowLutData(gdcm::File *f)
       else
          std::cout << "No LUT Data SeqEntry (0x0028,0x3000) found " 
                    << std::endl;
-
    }
 
 int main(int argc, char *argv[])
 {
 
    START_USAGE(usage)
-   " \n PrintFile : \n",
-   " Display the header of a ACR-NEMA/PAPYRUS/DICOM File",
-   " usage: PrintFile filein=fileName [level=n] [noshadowseq][noshadow][noseq][debug] ",
+   " \n PrintFile : \n                                                       ",
+   " Display the header of a ACR-NEMA/PAPYRUS/DICOM File                     ",
+   " usage: PrintFile {filein=inputFileName|dirin=inputDirectoryName}[level=n]", 
+   "                       [ { [noshadowseq] | [noshadow][noseq] } ] [debug]  ",
    "        level = 0,1,2 : depending on the amount of details user wants to see",
-   "        noshadowseq: user doesn't want to load Private Sequences",
+   "        noshadowseq: user doesn't want to load Private Sequences          ",
    "        noshadow   : user doesn't want to load Private groups (odd number)",
-   "        noseq      : user doesn't want to load Sequences ",
-   "        debug      : user wants to run the program in 'debug mode' ",
+   "        noseq      : user doesn't want to load Sequences                  ",
+   "        debug      : user wants to run the program in 'debug mode'        ",
    "        showlut :user wants to display the Palette Color (as an int array)",
    FINISH_USAGE
 
@@ -142,8 +143,24 @@ int main(int argc, char *argv[])
       return 0;
    }
 
-   char *fileName = am->ArgMgrWantString("filein",usage);
+   char *fileName = am->ArgMgrGetString("filein",(char *)0);
+   char *dirName  = am->ArgMgrGetString("dirin",(char *)0);
 
+   if ( (fileName == 0 && dirName == 0)
+        ||
+        (fileName != 0 && dirName != 0) )
+   {
+       std::cout <<std::endl
+                 << "Either 'filein=' or 'dirin=' must be present;" 
+                 << std::endl << "Not both" << std::endl;
+       am->ArgMgrUsage(usage); // Display 'usage'  
+       delete am;
+       return 0;
+ }
+
+   if (am->ArgMgrDefined("debug"))
+      gdcm::Debug::DebugOn();
+ 
    int loadMode = 0x00000000;
    if ( am->ArgMgrDefined("noshadowseq") )
       loadMode |= NO_SHADOWSEQ;
@@ -159,9 +176,6 @@ int main(int argc, char *argv[])
 
    bool showlut = ( 0 != am->ArgMgrDefined("SHOWLUT") );
 
-   if (am->ArgMgrDefined("debug"))
-      gdcm::Debug::DebugOn();
- 
    /* if unused Param we give up */
    if ( am->ArgMgrPrintUnusedLabels() )
    {
@@ -173,163 +187,211 @@ int main(int argc, char *argv[])
    delete am;  // we don't need Argument Manager any longer
 
    // ----------- End Arguments Manager ---------
- 
-   // gdcm::File::IsReadable() is no usable here, because we deal with
-   // any kind of gdcm-Parsable *document* 
-   // not only gdcm::File (as opposed to gdcm::DicomDir)
 
-   gdcm::File *f = new gdcm::File();
-   f->SetLoadMode(loadMode);
-   f->SetFileName( fileName );
-   bool res = f->Load();
+   if ( fileName != 0 ) // ====== Deal with a single file ======
+   { 
+      // gdcm::File::IsReadable() is no usable here, because we deal with
+      // any kind of gdcm-Parsable *document* 
+      // not only gdcm::File (as opposed to gdcm::DicomDir)
 
-   if ( !res )
-   {
-      std::cout << "Cannot process file [" << fileName << "]" << std::endl;
-      std::cout << "Either it doesn't exist, or it's read protected " 
-                << std::endl;
-      std::cout << "or it's not a Dicom File, or its 'header' is bugged" 
-                << std::endl;
-      std::cout << "use 'PrintFile filein=... debug' to try to guess the pb"
-                << std::endl;
-      delete f;
-      return 0;
-   }
+      gdcm::File *f = new gdcm::File();
+      f->SetLoadMode(loadMode);
+      f->SetFileName( fileName );
+      bool res = f->Load();
 
-   gdcm::FileHelper *fh = new gdcm::FileHelper(f);
-   fh->SetPrintLevel( level );
-
-   fh->Print();   
-
-   std::cout << "\n\n" << std::endl; 
-
-   std::cout <<std::endl;
-   std::cout <<" dataSize    " << fh->GetImageDataSize()    << std::endl;
-   std::cout <<" dataSizeRaw " << fh->GetImageDataRawSize() << std::endl;
-
-   int nX,nY,nZ,sPP,planarConfig;
-   std::string pixelType;
-   nX=f->GetXSize();
-   nY=f->GetYSize();
-   nZ=f->GetZSize();
-   std::cout << " DIMX=" << nX << " DIMY=" << nY << " DIMZ=" << nZ << std::endl;
-
-   pixelType    = f->GetPixelType();
-   sPP          = f->GetSamplesPerPixel();
-   planarConfig = f->GetPlanarConfiguration();
-   
-   std::cout << " pixelType= ["            << pixelType 
-             << "] SamplesPerPixel= ["     << sPP
-             << "] PlanarConfiguration= [" << planarConfig 
-             << "] "<< std::endl 
-             << " PhotometricInterpretation= [" 
-                                << f->GetEntryValue(0x0028,0x0004)
-             << "] "<< std::endl;
-
-   int numberOfScalarComponents=f->GetNumberOfScalarComponents();
-   std::cout << " NumberOfScalarComponents = " << numberOfScalarComponents 
-             <<std::endl
-             << " LUT = " << (f->HasLUT() ? "TRUE" : "FALSE")
-             << std::endl;
-
-   if ( f->GetEntryValue(0x0002,0x0010) == gdcm::GDCM_NOTLOADED ) 
-   {
-      std::cout << "Transfer Syntax not loaded. " << std::endl
-                << "Better you increase MAX_SIZE_LOAD_ELEMENT_VALUE"
-                << std::endl;
-      return 0;
-   }
-  
-   std::string transferSyntaxName = f->GetTransferSyntaxName();
-   std::cout << " TransferSyntaxName= [" << transferSyntaxName << "]" 
-             << std::endl;
-   std::cout << " SwapCode= " << f->GetSwapCode() << std::endl;
-
-   //std::cout << "\n\n" << std::endl; 
-   //std::cout << "X spacing " << f->GetXSpacing() << std::endl;
-   //std::cout << "Y spacing " << f->GetYSpacing() << std::endl;
-   //std::cout << "Z spacing " << f->GetZSpacing() << std::endl;
-
-   // Display the LUT as an int array (for debugging purpose)
-   if ( f->HasLUT() && showlut )
-   {
-      uint8_t* lutrgba = fh->GetLutRGBA();
-      if ( lutrgba == 0 )
+      if ( !res )
       {
-         std::cout << "Lut RGBA (Palette Color) not built " << std::endl;
- 
-        // Nothing is written yet to get LUT Data user friendly
-        // The following is to be moved into a PixelRedaConvert method
-  
-         gdcm::SeqEntry *modLutSeq = f->GetSeqEntry(0x0028,0x3000);
-         if ( modLutSeq !=0 )
-         {
-            gdcm::SQItem *sqi= modLutSeq->GetFirstSQItem();
-            if ( !sqi )
-            {
-               std::string lutDescriptor = sqi->GetEntryValue(0x0028,0x3002);
-               int length;   // LUT length in Bytes
-               int deb;      // Subscript of the first Lut Value
-               int nbits;    // Lut item size (in Bits)
-               int nbRead;    // nb of items in LUT descriptor (must be = 3)
+         std::cout << "Cannot process file [" << fileName << "]" << std::endl;
+         std::cout << "Either it doesn't exist, or it's read protected " 
+                   << std::endl;
+         std::cout << "or it's not a Dicom File, or its 'header' is bugged" 
+                   << std::endl;
+         std::cout << "use 'PrintFile filein=... debug' to try to guess the pb"
+                   << std::endl;
+         delete f;
+         return 0;
+      }
 
-               nbRead = sscanf( lutDescriptor.c_str(),
-                                 "%d\\%d\\%d",
-                                  &length, &deb, &nbits );
-               if ( nbRead != 3 )
+      gdcm::FileHelper *fh = new gdcm::FileHelper(f);
+      fh->SetPrintLevel( level );
+
+      fh->Print();
+
+      std::cout << "\n\n" << std::endl; 
+
+      std::cout <<std::endl;
+      std::cout <<" dataSize    " << fh->GetImageDataSize()    << std::endl;
+      std::cout <<" dataSizeRaw " << fh->GetImageDataRawSize() << std::endl;
+
+      int nX,nY,nZ,sPP,planarConfig;
+      std::string pixelType;
+      nX=f->GetXSize();
+      nY=f->GetYSize();
+      nZ=f->GetZSize();
+      std::cout << " DIMX=" << nX << " DIMY=" << nY << " DIMZ=" << nZ << std::endl;
+
+      pixelType    = f->GetPixelType();
+      sPP          = f->GetSamplesPerPixel();
+      planarConfig = f->GetPlanarConfiguration();
+
+      std::cout << " pixelType= ["            << pixelType 
+                << "] SamplesPerPixel= ["     << sPP
+                << "] PlanarConfiguration= [" << planarConfig 
+                << "] "<< std::endl 
+                << " PhotometricInterpretation= [" 
+                                << f->GetEntryValue(0x0028,0x0004)
+                << "] "<< std::endl;
+
+      int numberOfScalarComponents=f->GetNumberOfScalarComponents();
+      std::cout << " NumberOfScalarComponents = " << numberOfScalarComponents 
+                <<std::endl
+                << " LUT = " << (f->HasLUT() ? "TRUE" : "FALSE")
+                << std::endl;
+
+      if ( f->GetEntryValue(0x0002,0x0010) == gdcm::GDCM_NOTLOADED ) 
+      {
+         std::cout << "Transfer Syntax not loaded. " << std::endl
+                   << "Better you increase MAX_SIZE_LOAD_ELEMENT_VALUE"
+                << std::endl;
+         return 0;
+      }
+  
+      std::string transferSyntaxName = f->GetTransferSyntaxName();
+      std::cout << " TransferSyntaxName= [" << transferSyntaxName << "]" 
+                << std::endl;
+      std::cout << " SwapCode= " << f->GetSwapCode() << std::endl;
+
+      //std::cout << "\n\n" << std::endl; 
+      //std::cout << "X spacing " << f->GetXSpacing() << std::endl;
+      //std::cout << "Y spacing " << f->GetYSpacing() << std::endl;
+      //std::cout << "Z spacing " << f->GetZSpacing() << std::endl;
+
+      // Display the LUT as an int array (for debugging purpose)
+      if ( f->HasLUT() && showlut )
+      {
+         uint8_t* lutrgba = fh->GetLutRGBA();
+         if ( lutrgba == 0 )
+         {
+            std::cout << "Lut RGBA (Palette Color) not built " << std::endl;
+ 
+           // Nothing is written yet to get LUT Data user friendly
+           // The following is to be moved into a PixelRedaConvert method
+  
+            gdcm::SeqEntry *modLutSeq = f->GetSeqEntry(0x0028,0x3000);
+            if ( modLutSeq !=0 )
+            {
+               gdcm::SQItem *sqi= modLutSeq->GetFirstSQItem();
+               if ( !sqi )
                {
-                   //gdcmWarningMacro( "Wrong LUT descriptor" );
-                   std::cout << "Wrong LUT descriptor" << std::endl;
-               }                                                  
-               gdcm::BinEntry *b = sqi->GetBinEntry(0x0028,0x3006);
-               if ( b != 0 )
-               {
-                  if ( b->GetLength() != 0 )
+               std::string lutDescriptor = sqi->GetEntryValue(0x0028,0x3002);
+                  int length;   // LUT length in Bytes
+                  int deb;      // Subscript of the first Lut Value
+                  int nbits;    // Lut item size (in Bits)
+                  int nbRead;    // nb of items in LUT descriptor (must be = 3)
+
+                  nbRead = sscanf( lutDescriptor.c_str(),
+                                    "%d\\%d\\%d",
+                                     &length, &deb, &nbits );
+                  if ( nbRead != 3 )
                   {
-                     std::cout << "---------------------------------------"
+                      //gdcmWarningMacro( "Wrong LUT descriptor" );
+                      std::cout << "Wrong LUT descriptor" << std::endl;
+                  }                                                  
+                  gdcm::BinEntry *b = sqi->GetBinEntry(0x0028,0x3006);
+                  if ( b != 0 )
+                  {
+                     if ( b->GetLength() != 0 )
+                     {
+                        std::cout << "---------------------------------------"
                                << " We should never reach this point      "
                                << std::endl;
-                     //LoadEntryBinArea(b);    //LUT Data (CTX dependent)
-                  }   
-              }
-           }      
-         }
+                        //LoadEntryBinArea(b);    //LUT Data (CTX dependent)
+                     }   
+                 }
+              }      
+            }
+            else
+               std::cout << "No LUT Data (0x0028,0x3000) found " << std::endl;
+        }
          else
-             std::cout << "No LUT Data (0x0028,0x3000) found " << std::endl;
-     }
-      else
-      {
-         if ( fh->GetLutItemSize() == 8 )
          {
-            for (int i=0;i<fh->GetLutItemNumber();i++)
-               std::cout << i << " : \t"
+            if ( fh->GetLutItemSize() == 8 )
+            {
+               for (int i=0;i<fh->GetLutItemNumber();i++)
+                  std::cout << i << " : \t"
                          << (int)(lutrgba[i*4])   << " "
                          << (int)(lutrgba[i*4+1]) << " "
                          << (int)(lutrgba[i*4+2]) << std::endl;
-         }
-         else // LutItemSize assumed to be = 16
-         {
-            uint16_t* lutrgba16 = (uint16_t*)lutrgba;
-            for (int i=0;i<fh->GetLutItemNumber();i++)
-               std::cout << i << " : \t"
+            }
+            else // LutItemSize assumed to be = 16
+            {
+               uint16_t* lutrgba16 = (uint16_t*)lutrgba;
+               for (int i=0;i<fh->GetLutItemNumber();i++)
+                  std::cout << i << " : \t"
                          << (int)(lutrgba16[i*4])   << " "
                          << (int)(lutrgba16[i*4+1]) << " "
                          << (int)(lutrgba16[i*4+2]) << std::endl;
+            }
          }
       }
-   }
-   else if (showlut)
-   {
-      std::cout << "Try LUT Data "<< std::endl;
-      ShowLutData(f);
-   }
+      else if (showlut)
+      {
+         std::cout << "Try LUT Data "<< std::endl;
+         ShowLutData(f);
+      }
      
-   if (f->IsReadable())
-      std::cout <<std::endl<<fileName<<" is Readable"<<std::endl;
-   else
-      std::cout <<std::endl<<fileName<<" is NOT Readable"<<std::endl;
-   std::cout<<std::flush;
-   delete f;
-   delete fh;
-   return 0;   
+      if (f->IsReadable())
+         std::cout <<std::endl<<fileName<<" is Readable"<<std::endl;
+      else
+         std::cout <<std::endl<<fileName<<" is NOT Readable"<<std::endl;
+      std::cout<<std::flush;
+      delete f;
+      delete fh;
+      return 0;
+   }
+   else  // ====== Deal with a Directory ======
+   {
+      std::cout << "dirName [" << dirName << "]" << std::endl;
+      gdcm::DirList dirList(dirName,1); // gets recursively the file list
+      gdcm::DirListType fileList = dirList.GetFilenames();
+      gdcm::File *f;
+      bool res;
+      for( gdcm::DirListType::iterator it  = fileList.begin();
+                                 it != fileList.end();
+                                 ++it )
+      {
+         f = new gdcm::File();
+         f->SetLoadMode(loadMode);
+         f->SetFileName( it->c_str() );
+         res = f->Load();
+
+         if ( !res )
+         {
+            std::cout << "Cannot process file [" << it->c_str() << "]" 
+                      << std::endl;
+            std::cout << "Either it doesn't exist, or it's read protected " 
+                      << std::endl;
+            std::cout << "or it's not a Dicom File, or its 'header' is bugged" 
+                      << std::endl;
+            std::cout << "use 'PrintFile filein=... debug' to try to guess the pb"
+                   << std::endl;
+            delete f;
+            continue;
+         }
+
+         gdcm::FileHelper *fh = new gdcm::FileHelper(f);
+         fh->SetPrintLevel( level );
+
+         fh->Print();
+
+         if (f->IsReadable())
+            std::cout <<std::endl<<it->c_str()<<" is Readable"<<std::endl;
+         else
+            std::cout <<std::endl<<it->c_str()<<" is NOT Readable"<<std::endl;
+         std::cout << "\n\n" << std::endl;
+         delete f;
+         delete fh;
+      }
+      std::cout<<std::flush;
+   }
 }
