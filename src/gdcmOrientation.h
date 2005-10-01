c51e8fb561dad6c912762907f5107dba5f72afc8
malaterre <malaterre>
2005-10-01 15:51:42 +0000

    DOC: Adding comment by JPR, for non-medical user (=me)

--- src/gdcmOrientation.h
+++ src/gdcmOrientation.h
@@ -3,8 +3,8 @@
   Program:   gdcm
   Module:    $RCSfile: gdcmOrientation.h,v $
   Language:  C++
-  Date:      $Date: 2005/09/21 16:39:53 $
-  Version:   $Revision: 1.7 $
+  Date:      $Date: 2005/10/01 15:51:42 $
+  Version:   $Revision: 1.8 $
                                                                                 
   Copyright (c) CREATIS (Centre de Recherche et d'Applications en Traitement de
   l'Image). All rights reserved. See Doc/License.txt or
@@ -37,6 +37,44 @@ class File;
 //-----------------------------------------------------------------------------
 /**
  * \brief Orientation class for dealing with DICOM image orientation
+ * A gentle reminder for non-medical user:
+ * PatientPosition (0x0010,0x5100) tells us the way the patient was introduced in the imager
+ *  - HFS : Head First Supine
+ *  - FFS : Feet First Supine
+ *  - HFP : Head First Prone
+ *  - FFP : Feet First Prone
+ * Note: HFP and FFP are not very common values, since the position must be pretty unconfortable for the Patient -the patient is lying on his belly; but, if he has handcuffs there is no other way ...-
+ *
+ * ImageOrientationPatient (0x0020,0x0037) gives 6 cosines (2 for each plane)
+ * Patient Orientation (as found in the optional 0x0020,0x0020, or computed by
+ * std::string Orientation::GetOrientation ( File *f ), tells us about the direction of X and Y axes.
+ * 
+ * The values can be
+ *  - A/P anterior/posterior
+ *  - L/R left/right
+ *  - H/F head/feet
+ * One can see it as "values within a 'Patient referential".
+ *
+ * Example #1:
+ * Imagine the patient, in "HFS" position.
+ * Sagital images are requested.
+ * All the cosines will be -1, 0, or +1;
+ * "Patient Orientation" (deduced) will be "A/F".
+ * Positive X axis is oriented 'towards patient's nose
+ * Positive Y axis  is oriented 'towards patient's feet
+ *
+ * Example #2:
+ * Imagine now that patient has a stiffneck and his head is *turned* 30 degrees towards the left.
+ * Sagital images are requested.
+ * One of the cosines will be almost 0.5
+ * Deduced "Patient Orientation" will be "AL\F"
+ * (main X axis orientation is towards patient's nose, and a little bit towards the left)
+ * but the image is *perfectly* sagital !
+ *
+ * Imagine the patient's stiffneck causes head to be *bended* 30 degrees towards the left AND *turned* left.
+ * Sagital images are requested...
+ * You'll probabely have 3 letters for X axis and  Y axis, and the image remains *perfectly* sagital !
+ * The values are given within the 'imager referential', better than within the 'Patient Referential' ...
  */
 class GDCM_EXPORT Orientation : public Base
 {
