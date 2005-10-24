abe1322f081be85f75261679b1b945b1e31c88c5
malaterre <malaterre>
2005-10-24 15:41:55 +0000

    STYLE: Cleanup code

--- src/gdcmJpeg2000.cxx
+++ src/gdcmJpeg2000.cxx
@@ -3,8 +3,8 @@
   Program:   gdcm
   Module:    $RCSfile: gdcmJpeg2000.cxx,v $
   Language:  C++
-  Date:      $Date: 2005/10/24 15:36:33 $
-  Version:   $Revision: 1.29 $
+  Date:      $Date: 2005/10/24 15:41:55 $
+  Version:   $Revision: 1.30 $
                                                                                 
   Copyright (c) CREATIS (Centre de Recherche et d'Applications en Traitement de
   l'Image). All rights reserved. See Doc/License.txt or
@@ -20,9 +20,9 @@
 
 #include <iostream>
 #include <fstream>
-//#include <jasper/jasper.h>
+
 extern "C" {
-#include <openjpeg.h>
+  #include <openjpeg.h>
 }
 
 namespace gdcm 
@@ -40,7 +40,6 @@ bool gdcm_read_JPEG2000_file (void* raw, char *inputdata, size_t inputlength)
 {
   j2k_image_t img;
   j2k_cp_t cp;
-  //jp2_struct_t *jp2_struct=NULL;
 
   // default blindly copied
   cp.layer=0;
@@ -53,87 +52,62 @@ bool gdcm_read_JPEG2000_file (void* raw, char *inputdata, size_t inputlength)
   int len = inputlength;
   unsigned char *src = (unsigned char*)inputdata;
 
-    if (!j2k_decode(src, len, &img, &cp)) {
-      fprintf(stderr, "ERROR -> j2k_to_image: failed to decode image!\n");
-      return false;
-    }
+  // Decompression
+  if (!j2k_decode(src, len, &img, &cp)) {
+    fprintf(stderr, "ERROR -> j2k_to_image: failed to decode image!\n");
+    return false;
+  }
 
+  // Copy buffer
+  for (int compno = 0; compno < img.numcomps; compno++)
+  {
+    j2k_comp_t *comp = &img.comps[compno];
+    int nbytes = 0;
 
-    /************** PGX *****************/
-    //char *data8 = (char*)raw;
-    //uint16_t *data8 = (uint16_t*)raw;
-   for (int compno = 0; compno < img.numcomps; compno++) {
-      j2k_comp_t *comp = &img.comps[compno];
-      int nbytes = 0;
- 
-      // w = int_ceildiv(img.x1 - img.x0, comp->dx);
-      // wr = int_ceildiv(int_ceildivpow2(img.x1 - img.x0,img.factor), comp->dx);
-      int w = img.comps[compno].w;
-      int wr = int_ceildivpow2(img.comps[compno].w, img.comps[compno].factor);
-      std::cerr << "wr=" << wr << std::endl;
-      
-      // h = int_ceildiv(img.y1 - img.y0, comp->dy);
-      // hr = int_ceildiv(int_ceildivpow2(img.y1 - img.y0,img.factor), comp->dy);
-      int h = img.comps[compno].h;
-      int hr = int_ceildivpow2(img.comps[compno].h, img.comps[compno].factor);
-      std::cerr << "hr=" << hr << std::endl;
-      
-      //fprintf(fdest, "PG ML %c %d %d %d\n", comp->sgnd ? '-' : '+',
-      //  comp->prec, wr, hr);
+    int w = img.comps[compno].w;
+    int wr = int_ceildivpow2(img.comps[compno].w, img.comps[compno].factor);
 
-      if (comp->prec <= 8)
+    //int h = img.comps[compno].h;
+    int hr = int_ceildivpow2(img.comps[compno].h, img.comps[compno].factor);
+
+    if (comp->prec <= 8)
+      {
+      nbytes = 1;
+      uint8_t *data8 = (uint8_t*)raw;
+      for (int i = 0; i < wr * hr; i++) 
         {
-        nbytes = 1;
-        uint8_t *data8 = (uint8_t*)raw;
-        for (int i = 0; i < wr * hr; i++) 
-          {
-          int v = img.comps[compno].data[i / wr * w + i % wr];
-          *data8++ = (uint8_t)v;
-          }
+        int v = img.comps[compno].data[i / wr * w + i % wr];
+        *data8++ = (uint8_t)v;
         }
-      else if (comp->prec <= 16)
+      }
+    else if (comp->prec <= 16)
+      {
+      nbytes = 2;
+      uint16_t *data16 = (uint16_t*)raw;
+      for (int i = 0; i < wr * hr; i++) 
         {
-        nbytes = 2;
-        uint16_t *data16 = (uint16_t*)raw;
-        for (int i = 0; i < wr * hr; i++) 
-          {
-          int v = img.comps[compno].data[i / wr * w + i % wr];
-          *data16++ = (uint16_t)v;
-          }
+        int v = img.comps[compno].data[i / wr * w + i % wr];
+        *data16++ = (uint16_t)v;
         }
-      else
+      }
+    else
+      {
+      nbytes = 4;
+      uint32_t *data32 = (uint32_t*)raw;
+      for (int i = 0; i < wr * hr; i++) 
         {
-        nbytes = 4;
-        uint32_t *data32 = (uint32_t*)raw;
-        for (int i = 0; i < wr * hr; i++) 
-          {
-          int v = img.comps[compno].data[i / wr * w + i % wr];
-          *data32++ = (uint32_t)v;
-          }
+        int v = img.comps[compno].data[i / wr * w + i % wr];
+        *data32++ = (uint32_t)v;
         }
-//      for (int i = 0; i < wr * hr; i++) 
-//        {
-//        //std::cout << "i:" << i << '\n';
-//        int v = img.comps[compno].data[i / wr * w + i % wr];
-//        //for (int j = nbytes - 1; j >= 0; j--) 
-//        for (int j = 0; j < nbytes-1 ; j++) 
-//          {
-//          char byte = (char) (v >> (j * 8));
-//          //fwrite(&byte, 1, 1, fdest);
-//          *data8++ = byte;
-//        }
-//        //*data8++ = (uint16_t)v;
-//      }
-      free(img.comps[compno].data);
-//      fclose(fdest);
-    /************** END PGX *****************/
-   }
+      }
+    free(img.comps[compno].data);
+  }
 
 
   // Free remaining structures
-  //--------------------------
-  delete[] inputdata;
   j2k_dec_release();
+  // FIXME
+  delete[] inputdata;
 
   return true;
 }
