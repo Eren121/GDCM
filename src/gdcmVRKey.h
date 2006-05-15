5507525c9f8cfcab45e7f9ad684a4a825ec86b16
malaterre <malaterre>
2006-05-15 14:47:13 +0000

    COMP: Fix comp on dash8 (gcc296)

--- src/gdcmVRKey.h
+++ src/gdcmVRKey.h
@@ -3,8 +3,8 @@
   Program:   gdcm
   Module:    $RCSfile: gdcmVRKey.h,v $
   Language:  C++
-  Date:      $Date: 2005/10/23 15:01:34 $
-  Version:   $Revision: 1.5 $
+  Date:      $Date: 2006/05/15 14:47:13 $
+  Version:   $Revision: 1.6 $
                                                                                 
   Copyright (c) CREATIS (Centre de Recherche et d'Applications en Traitement de
   l'Image). All rights reserved. See Doc/License.txt or
@@ -23,6 +23,7 @@
 
 #include <assert.h>
 #include <iomanip> // important
+#include <string>
 
 namespace gdcm 
 {
