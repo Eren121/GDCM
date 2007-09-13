97b89d130dcfb43aff4c742e0d0d08f159ad3b4d
malaterre <malaterre>
2007-09-13 12:48:37 +0000

    ENH: add copyright

--- Example/toBrainVisa.cxx
+++ Example/toBrainVisa.cxx
@@ -1,3 +1,20 @@
+/*=========================================================================
+                                                                                
+  Program:   gdcm
+  Module:    $RCSfile: toBrainVisa.cxx,v $
+  Language:  C++
+  Date:      $Date: 2007/09/13 12:48:37 $
+  Version:   $Revision: 1.3 $
+                                                                                
+  Copyright (c) CREATIS (Centre de Recherche et d'Applications en Traitement de
+  l'Image). All rights reserved. See Doc/License.txt or
+  http://www.creatis.insa-lyon.fr/Public/Gdcm/License.html for details.
+                                                                                
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+                                                                                
+=========================================================================*/
 #include "gdcmFile.h"
 #include "gdcmFileHelper.h"
 #include "gdcmDocument.h"
